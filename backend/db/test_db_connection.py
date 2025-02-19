6f368c77c02e45c6639a1bc171251a3a49d05d70
João Paulo Souza <56564225+akanowhere@users.noreply.github.com>
2025-02-19 18:24:36 +0000

    teste

--- backend/db/test_db_connection.py
+++ backend/db/test_db_connection.py
@@ -5,9 +5,9 @@ import requests
 
 # URL de conexão com o banco de dados
 #DATABASE_URL = "postgresql://postgres:tlaush2020@192.168.15.5:5432/sat_system_db"
-DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
+#DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
 #DATABASE_URL = "postgresql://postgres:tlaush2020@[2804:7f0:b84d:7001:2cac:75eb:17ba:90]:5432/sat_system_db"
-#DATABASE_URL = "postgresql://neondb_owner:npg_uOAVw76PYpTD@ep-black-butterfly-a87ca2ez-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_uOAVw76PYpTD@ep-black-butterfly-a87ca2ez-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 
 
 # Obtém o IP público
