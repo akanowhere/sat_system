1ea23ed7c414a3c56cf4622800c40eab005a1057
João Paulo Souza <56564225+akanowhere@users.noreply.github.com>
2025-02-20 19:01:06 +0000

    url

--- frontend/screens/pedidos.py
+++ frontend/screens/pedidos.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 
 
-API_URL = "http://localhost:8000/pedidos/"
-#API_URL = "https://humble-yodel-57qvr7v97fvg4r-8000.app.github.dev/pedidos/"
+#API_URL = "http://localhost:8000/pedidos/"
+API_URL = "https://humble-yodel-57qvr7v97fvg4r-8000.app.github.dev/pedidos/"
 #API_URL = "https://satsystem.streamlit.app/pedidos/"
 
 def exibir_pedidos():
