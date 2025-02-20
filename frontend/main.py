0aa183770f58b5f326af69a5be634e3972149e62
João Paulo Souza <56564225+akanowhere@users.noreply.github.com>
2025-02-20 12:11:12 +0000

    text

--- frontend/main.py
+++ frontend/main.py
@@ -12,13 +12,13 @@ import streamlit as st
 from frontend.screens.pedidos import exibir_pedidos, adicionar_pedido
 
 def main():
-    st.title("Sistema de Pedidos")
+    st.title("Sistema de Pedidos SAT")
 
     menu = ["Home", "Pedidos", "Pagamentos"]
     escolha = st.sidebar.selectbox("Escolha a Tela", menu)
 
     if escolha == "Home":
-        st.subheader("Bem-vindo ao Sistema de Pedidos!")
+        st.subheader("Sistema Autenticador e Transmissor de Cupons Fiscais Eletrônicos")
     elif escolha == "Pedidos":
         exibir_pedidos()
         adicionar_pedido()
