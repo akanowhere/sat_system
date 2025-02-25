0956ec791dd0c73f8b75596fd0ec846a51dce66b
João Paulo Souza <56564225+akanowhere@users.noreply.github.com>
2025-02-25 12:58:15 +0000

    ad_auth

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from backend.api.sat import router as sat_router
 from backend.api.pedidos import router as pedidos_router
 from backend.api.pagamentos import router as pagamentos_router
+from backend.api.cadastros import router as cadastros_router
 
 # Criação da instância FastAPI
 app = FastAPI()
@@ -12,6 +13,8 @@ app = FastAPI()
 app.include_router(sat_router, prefix="/sat", tags=["sat"])
 app.include_router(pedidos_router, prefix="/pedidos", tags=["pedidos"])
 app.include_router(pagamentos_router, prefix="/pagamentos", tags=["pagamentos"])
+app.include_router(cadastros_router, prefix="/cadastros", tags=["cadastros"])
+app.include_router(cadastros_router, prefix="/auth", tags=["auth"])
 
 @app.get("/")
 def read_root():
