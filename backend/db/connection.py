5358ba46de2f7b425dce7d0fb6f4878c1ee9eb0d
João Paulo Souza <56564225+akanowhere@users.noreply.github.com>
2025-05-23 12:57:22 +0000

    fix: ip connection

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker
 #DATABASE_URL = "postgresql://usuario:senha@localhost:5432/sat_system_db"  # Substitua pelo seu usuário e senha
 #DATABASE_URL = "postgresql://postgres:tlaush2020@localhost:5432/sat_system_db"  # Substitua pelo seu usuário e senha
 #DATABASE_URL = "postgresql://postgres:tlaush2020@192.168.15.5:5432/sat_system_db"
-DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
+#DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
+DATABASE_URL = "postgresql://postgres:tlaush2020@201.6.116.115:5432/sat_system_db"
 #DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
 #DATABASE_URL = "postgresql://neondb_owner:npg_uOAVw76PYpTD@ep-black-butterfly-a87ca2ez-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 
--- backend/db/test_db_connection.py
+++ backend/db/test_db_connection.py
@@ -5,16 +5,17 @@ import requests
 
 # URL de conexão com o banco de dados
 #DATABASE_URL = "postgresql://postgres:tlaush2020@192.168.15.3:5432/sat_system_db"
-DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
+#DATABASE_URL = "postgresql://postgres:tlaush2020@191.9.122.99:5432/sat_system_db"
+DATABASE_URL = "postgresql://postgres:tlaush2020@201.6.116.115:5432/sat_system_db"
 #DATABASE_URL = "postgresql://postgres:tlaush2020@[2804:7f0:b84d:7001:2cac:75eb:17ba:90]:5432/sat_system_db"
 #DATABASE_URL = "postgresql://neondb_owner:npg_uOAVw76PYpTD@ep-black-butterfly-a87ca2ez-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 
 
 # Obtém o IP público
-ip_publico = requests.get('https://api.ipify.org').text
+#ip_publico = requests.get('https://api.ipify.org').text
 
 # Exibe o IP público no Streamlit
-print(ip_publico)
+#print(ip_publico)
 
 
 
